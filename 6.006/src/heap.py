246625f7c3c01bc163aa3d635dc6b9c0d6ce4cab
W Lim <64784258+wolimst@users.noreply.github.com>
2020-08-05 00:15:35 +0900

    Add insert(), get_key(). Update variable names and comments.

--- 6.006/src/heap.py
+++ 6.006/src/heap.py
@@ -1,9 +1,11 @@
+from __future__ import annotations
 import math
 
 
-class Heap:
+class MaxHeap:
     def __init__(self, heap):
         self.heap = list(heap)
+        self.build_max_heap()
 
     def __str__(self):
         return str(self.heap)
@@ -11,19 +13,42 @@ class Heap:
     def __len__(self):
         return len(self.heap)
 
-    def build_max_heap(self):
+    def get_key(self, index: int):
+        return self.heap[index]
+
+    def insert(self, key) -> MaxHeap:
+        """
+        Insert a key into the max heap.
+
+        Complexity: O(lg(n))
+        """
+        i = len(self.heap)
+        self.heap.append(key)
+        while i > 0:
+            parent_index = (i + 1) // 2 - 1
+            if self.heap[i] > self.heap[parent_index]:
+                self.heap[i], self.heap[parent_index] = (
+                    self.heap[parent_index],
+                    self.heap[i],
+                )
+                i = parent_index
+            else:
+                break
+        return self
+
+    def build_max_heap(self) -> None:
         """
         Produces a max heap from an unordered array.
 
         Complexity: O(n) where n is the size of heap
-                    because level m has 2^m nodes and the complexity of
-                    max_heapify() of a node on level m is O(lg(n) - m)
+                    because there are 2^m nodes on level m and the complexity
+                    of max_heapify() of a node on level m is O(lg(n) - m).
         """
         max_non_leaf_index = len(self.heap) // 2 - 1
         for i in range(max_non_leaf_index, -1, -1):
             self.max_heapify(i)
 
-    def max_heapify(self, i: int):
+    def max_heapify(self, i: int) -> None:
         """
         Correct a single violation of the heap property in a subtree's root.
 
@@ -32,14 +57,14 @@ class Heap:
                     is i.
         """
         try:
-            left_heap_index = self.get_left_heap_index_of(i)
-            left_child = self.heap[left_heap_index]
+            left_child_index = self.get_left_child_index(i)
+            left_child = self.heap[left_child_index]
         except IndexError:
             left_child = -math.inf
 
         try:
-            right_heap_index = self.get_right_heap_index_of(i)
-            right_child = self.heap[right_heap_index]
+            right_child_index = self.get_right_child_index(i)
+            right_child = self.heap[right_child_index]
         except IndexError:
             right_child = -math.inf
 
@@ -47,31 +72,25 @@ class Heap:
             return
 
         if left_child >= right_child:
-            j = left_heap_index
+            j = left_child_index
         else:
-            j = right_heap_index
+            j = right_child_index
 
         if self.heap[i] < self.heap[j]:
             self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
 
         self.max_heapify(j)
 
-    def get_left_heap_index_of(self, i: int) -> int:
-        """
-        Complexity: O(1)
-        """
-        child_index = i * 2 + 1
+    def get_left_child_index(self, index: int) -> int:
+        child_index = index * 2 + 1
         if not self.index_exists(child_index):
-            raise IndexError(f"Index {i} is a leaf.")
+            raise IndexError(f"Index {index} is out of range.")
         return child_index
 
-    def get_right_heap_index_of(self, i: int) -> int:
-        """
-        Complexity: O(1)
-        """
-        child_index = i * 2 + 2
+    def get_right_child_index(self, index: int) -> int:
+        child_index = index * 2 + 2
         if not self.index_exists(child_index):
-            raise IndexError(f"Index {i} is a leaf.")
+            raise IndexError(f"Index {index} is out of range.")
         return child_index
 
     def index_exists(self, i: int) -> bool:
@@ -84,26 +103,13 @@ class Heap:
         """
         Complexity: O(n lg(n))
         """
-
-        self.build_max_heap()
         heap_copy = self.heap.copy()
 
-        sorted_desc = []
+        desc_sorted_list = []
         while len(self.heap):
             self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
-            sorted_desc.append(self.heap.pop())
+            desc_sorted_list.append(self.heap.pop())
             self.max_heapify(0)
 
         self.heap = heap_copy
-        return sorted_desc
-
-
-if __name__ == "__main__":
-    heap = Heap([1, 2, 3, 4, 5, 6, 7, 8, 9])
-    print(heap)
-
-    heap.build_max_heap()
-    print(heap)
-
-    sorted = heap.sort()
-    print(sorted)
+        return desc_sorted_list
